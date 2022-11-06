2e2eaa3048398ed6e1dd1a4961acaa0c60e1a394
manager <admin@mindmap.ai>
2022-11-06 09:38:51 +0900

    'roomdetail'

--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from django.db import transaction
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound, NotAuthenticated
+from rest_framework.exceptions import (
+    NotFound, 
+    NotAuthenticated, 
+    ParseError,
+    PermissionDenied,
+)
 from .models import Amenity, Room
+from categoriesConfig.models import Category
 from .serializers import AmenitySerializer, RoomDetailSerializer, RoomListSerializer
 
 
@@ -66,17 +73,49 @@ class Rooms(APIView):
         return Response(serializer.data)
 
     def post(self, request):
-        serializer = RoomDetailSerializer(data=request.data)
-        if serializer.is_valid():
-            room = serializers.save()
-            serializer = RoomDetailSerializer(room)
-            return Response(serializer.data)
+        # serializer = RoomDetailSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     room = serializers.save()
+        #     serializer = RoomDetailSerializer(room)
+        #     return Response(serializer.data)
         if request.user.is_authenticated:
             serializer = RoomDetailSerializer(data=request.data)
             if serializer.is_valid():
-                room = serializer.saver(owner=request.user)
-                serializer = RoomDetailSerializer(room)
-                return Response(serializer.data)
+                room = serializer.save(owner=request.user)
+                category_pk = request.data.get("categoty")
+                if not category_pk:
+                    raise ParseError("Category is required.")
+                try:
+                    category = Category.objects.get(pk=category_pk)
+                    if category.kind == Category.CategoryKindChoices.EXPERIENCES:
+                        raise ParseError("The Category kind should be 'rooms'")
+                except Category.DoesNotExist:
+                    raise ParseError("category not found")
+                
+                try:
+                    with transaction.atomic():
+                        room = serializer.save(
+                            owner = request.user,
+                            category=category,
+                        )
+                        amenities = request.data.get("amenities")
+                        for amenity_pk in amenities:
+                            amenity = Amenity.objects.get(pk=amenity_pk)
+                            room.amenities.add(amenity)
+                        serializer = RoomDetailSerializer(room)
+                        return Response(serializer.data)
+                except Exception:
+                    raise ParseError("Amenity not found")
+                # for amenity_pk in amenities:
+                #     try:
+                #         amenity = Amenity.objects.get(pk=amenity_pk)
+                #     except Amenity.DoesNotExist:
+                #         room.delete()
+                #         raise ParseError(f"Amenity with id {amenity_pk} not found")
+                #     room.amenities.add(amenity)
+                # serializer = RoomDetailSerializer(room)
+                # return Response(serializer.data)
+
             else:
                 return Response(serializer.errors)
         else:
@@ -91,4 +130,14 @@ class RoomDetail(APIView):
     def get(self, request, pk):
         room = self.get_object(pk)
         serializer = RoomDetailSerializer(room)
-        return Response(serializer.data)         
+        return Response(serializer.data)
+
+    
+    def delete(self, request, pk):
+        room = self.get_object(pk)
+        if not request.user.is_authenticated:
+            raise NotAuthenticated
+        if room.owner != request.user:
+            raise PermissionDenied
+        room.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
